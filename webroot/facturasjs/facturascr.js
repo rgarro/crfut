e1fad59420d16ce191cb5d97c7fc7a4124821d5b
rgarro <rgarro@gmail.com>
2017-10-17 16:56:41 -0600

    [Delivers #152053593] translation sys shai logo update

---
+++ webroot/facturasjs/facturascr.js
@@ -0,0 +1,22 @@
+var FacturasCR = (function(){
+
+  function FacturasCR(){
+    this.mesh = null;
+    this.gameIsSet = false;
+  }
+
+  FacturasCR.prototype.loadModel = function(modelUrl){
+
+  }
+
+  FacturasCR.UrlVars = function() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    vars[key] = value;
+    });
+    return vars;
+  }
+  
+  return FacturasCR;
+})();
+CRFut.FacturasCR = FacturasCR;
