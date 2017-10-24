c43c6c00f7e5a3989c57aa06619743364b0d156c
rgarro <rgarro@gmail.com>
2017-10-24 11:22:22 -0600

    [#151689679] component failing tests

---
+++ src/Controller/Component/BettyChecksComponent.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Controller\ComponentRegistry;
+
+/**
+ * BettyChecks component
+ */
+class BettyChecksComponent extends Component
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+}
---
+++ tests/TestCase/Controller/Component/BettyChecksComponentTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Test\TestCase\Controller\Component;
+
+use App\Controller\Component\BettyChecksComponent;
+use Cake\Controller\ComponentRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\Component\BettyChecksComponent Test Case
+ */
+class BettyChecksComponentTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \App\Controller\Component\BettyChecksComponent
+     */
+    public $BettyChecks;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $registry = new ComponentRegistry();
+        $this->BettyChecks = new BettyChecksComponent($registry);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->BettyChecks);
+
+        parent::tearDown();
+    }
+
+    public function testHasVeryToken()
+    {
+        $this->assertTrue(method_exists($this->BettyChecks,'veryToken'));
+    }
+
+    public function testHasSessions()
+    {
+        $this->assertTrue(property_exists($this->BettyChecks,'Sessions'));
+    }
+
+    public function testHasLastCheckResult()
+    {
+        $this->assertTrue(property_exists($this->BettyChecks,'LastCheckResult'));
+        $this->assertTrue(is_array($this->BettyChecks->LastCheckResult));
+    }
+
+    public function testVeryTokenInvalidToken(){
+      $invalid_token = "95oor8Retg3i64u3t7Da59ee759cc04ed5.17119151";
+      $this->BettyChecks->veryToken($invalid_token);
+      $this->assertFalse($this->BettyChecks->LastCheckResult['is_alive']);
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization()
+    {
+        $this->assertTrue(true);
+    }
+}
