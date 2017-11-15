14cc601f1aa31bb9316bf442284976aadbb5b18c
rgarro <rgarro@gmail.com>
2017-11-14 21:27:37 -0600

    [Finish #152828170] companies datatable

--- src/Controller/CompaniesController.php
+++ src/Controller/CompaniesController.php
@@ -12,5 +12,44 @@ use App\Controller\AppController;
 class CompaniesController extends AppController
 {
 
+  public function initialize(){
+      parent::initialize();
+      $this->loadModel("Users");
+      $this->loadModel("Companies");
+      $this->loadModel("Sessions");
+      $this->loadComponent("BettyChecks");
+      $this->loadComponent("DataTablePastry");
+  }
+
+  public function datatable(){
+    if(isset($_GET["token"])){
+      $this->BettyChecks->veryToken($_GET["token"]);
+      if($this->BettyChecks->LastCheckResult["is_alive"]){
+        $user_id = intval(base64_decode($_GET['user_id']));
+
+        $searchables = $this->DataTablePastry->paramFilterSearchableColumnNames($_POST['columns']);
+        $sortables = $this->DataTablePastry->paramFilterSortableColumnNames($_POST['columns']);
+        $res = $this->Companies->dataTableData($user_id,$_POST['length'],$_POST['start'],$_POST['search']['value'],$searchables,$sortables,$_POST['order'][0]['dir']);
+        $ret = [
+            "draw"=> intval($_POST['draw']),
+            "recordsTotal" => $res['total'],
+            "recordsFiltered" => $res['total'],
+            "data" => $res['data']
+          ];
+
+      }else{
+        $ret = [];
+        $ret["token_is_expired"] = 1;
+        //throw new Exception("token is expired");//ajax logout callback will be ...
+      }
+
+    }else{
+      $ret = [];
+      $ret["token_is_absent"] = 1;
+      //throw new Exception("token is required");
+    }
+    $this->cors_here();
+    $this->set($ret);
+  }
 
 }
